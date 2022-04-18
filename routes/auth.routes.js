0fe7eabc771d50b77e6dd25ad00832881c5c86c0
highlyS-king <>
2022-04-18 16:44:32 +0800

    adding headers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,6 +6,10 @@ module.exports = function(app) {
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
     );
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
     next();
   });
   app.post(
